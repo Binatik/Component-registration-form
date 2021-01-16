13c403ecda89faa60b0df2da4211df3162a4ccce
Binatik <47430210+Binatik@users.noreply.github.com>
2021-01-16 19:59:26 +0300

    add style Form

--- src/components/form/ComponentForm/Input.js
+++ src/components/form/ComponentForm/Input.js
@@ -3,14 +3,14 @@ import React from 'react';
 // =================================================
 
 // Component
-//import './registration.css';
+import './input.css';
 
 function Input() {
     return (
         <React.Fragment>
             <div className="form__group">
                 <h2 className="form__title">Имя</h2>
-                <input type="text" value="Очистить" />
+                <input className="form__input" type="text" />
             </div>
         </React.Fragment>
     );
