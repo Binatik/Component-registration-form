4b22bc92fe94215ab44b3aa004d89c2546ed4e14
Binatik <47430210+Binatik@users.noreply.github.com>
2021-01-16 20:04:11 +0300

    Status Error

--- src/components/form/ComponentForm/Input.js
+++ src/components/form/ComponentForm/Input.js
@@ -11,6 +11,7 @@ function Input() {
             <div className="form__group">
                 <h2 className="form__title">Имя</h2>
                 <input className="form__input" type="text" />
+                <span className="form__input_error">Введено не корректное значение</span>
             </div>
         </React.Fragment>
     );
